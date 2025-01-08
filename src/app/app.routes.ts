e1f880de65aa707830b94307fee51cd91dc32b6b
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-08 18:46:31 -0300

    criando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home/home-page/home-page.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full' , component: HomePageComponent }
+    { path: '', pathMatch: 'full' , component: HomePageComponent },
+    { path: 'sobre-nos', component: AboutComponent }
 ];
