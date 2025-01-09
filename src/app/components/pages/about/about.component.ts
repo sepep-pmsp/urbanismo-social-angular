e579ada5fdcca0f6a25c6bc0c130d863815afad7
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-08 21:06:25 -0300

    criando pagina sobre

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { StickyHoldComponent } from "../../home/sticky-hold/sticky-hold.component";
+import { NgFor } from '@angular/common';
+import { title } from 'process';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [StickyHoldComponent, NgFor],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  repeatCount = 10;
 
+  apresentacao = [
+    {
+      txt_1: 'O urbanismo social é uma estratégia de intervenção urbana que, a partir da articulação das políticas públicas municipais, urbanização de espaços públicos livres e participação social, tem como principal objetivo a qualificação de territórios com altos índices vulnerabilidade e a promoção de espaços acolhedores, resilientes e seguros, de modo a reduzir as desigualdades socioespaciais no município.', 
+      txt_2: 'Os projetos são únicos, elaborados para atender as necessidades particulares diagnosticadas em cada território. Eles passam por diversas fases, tendo início com a construção de um Diagnóstico Técnico-Participativo e Plano de Ação, resultando em projetos de intervenção de espaços públicos e na implementação das políticas públicas integradas no território. Veja abaixo os eixos de atuação.'
+    },
+  ];
+
+  eixos=[
+    {
+      title: 'eixo de atuação',
+      img: 'assets/images/EIXOS.svg',
+      alt: 'Eixos de atuação',
+      list: [
+        {txt: '01. cidadania'},
+        {txt: '02. oportunidade'},
+        {txt: '03. sustentabilidade'},
+        {txt: '04. moradia'}
+      ]
+    }
+  ];
+
+  video: { title: string; video: SafeResourceUrl; type: string }[] = [];
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.video = [
+      {
+        title: 'vídeo institucional',
+        video: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/DzssQHFaoV4'),
+        type: 'video/youtube'
+      }
+    ];
+  }
 }
