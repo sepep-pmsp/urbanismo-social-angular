d414fc054ccd0c2328dcdd3aae80d3f0781c337d
Ana Carolina <carol00_10@hotmail.com>
2025-01-13 17:08:53 -0300

    new component

---
+++ src/app/components/pages/governanca/governanca.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-governanca',
+  imports: [RouterModule],
+  templateUrl: './governanca.component.html',
+  styleUrl: './governanca.component.scss'
+})
+export class GovernancaComponent {
+
+}
