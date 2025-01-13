088818f72479f946dba886ce0970eaa83bcd1c4f
Ana Carolina <carol00_10@hotmail.com>
2025-01-13 17:08:42 -0300

    ajustando as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home/home-page/home-page.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { GovernancaComponent } from './components/pages/governanca/governanca.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full' , component: HomePageComponent },
-    { path: 'sobre-nos', component: AboutComponent }
+    { path: 'sobre-nos', component: AboutComponent },
+    { path: 'governanca', component: GovernancaComponent },
 ];
