5125b36de22fe4b423c5a74388941de176bf0a27
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-14 11:08:33 -0300

    create page 404 not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './components/home/home-page/home-page.compone
 import { AboutComponent } from './components/pages/about/about.component';
 import { GovernancaComponent } from './components/pages/governanca/governanca.component';
 import { PartnersComponent } from './components/pages/partners/partners.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full' , component: HomePageComponent },
+    { path: '' , component: HomePageComponent },
     { path: 'sobre-nos', component: AboutComponent },
     { path: 'governanca', component: GovernancaComponent },
     { path: 'parceiros', component: PartnersComponent },
+    { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
