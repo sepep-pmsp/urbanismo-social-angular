b8c4207fa58d232fe416e098dcbf250f2d871f33
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-02-11 15:44:49 -0300

    create page news

---
+++ src/app/components/pages/news/news-page/news-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { News, ServiceNews } from '../service-news.service';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-news-page',
+  imports: [RouterModule, BrowserModule, HttpClientModule],
+  templateUrl: './news-page.component.html',
+  styleUrl: './news-page.component.scss'
+})
+export class NewsPageComponent  implements OnInit{
+  newsItem?: News;
+
+  constructor(private route: ActivatedRoute, private newsService: ServiceNews) {}
+
+  ngOnInit(): void {
+    const url = this.route.snapshot.paramMap.get('url');
+    this.newsService.getNews().subscribe((news) => {
+      this.newsItem = news.find((item) => item.url === url);
+    });
+  }
+}
